35916f5d3a92fd9f23a4f3a6291f5827ddc44764
Dustin Teel <dustinteel@Dustins-MacBook-Pro-2.local>
2017-02-14 15:16:10 -0500

    Game skeleton

---
+++ TaskIt.py
@@ -0,0 +1,31 @@
+import pygame
+ 
+# initialize game engine
+pygame.init()
+# set screen width/height and caption
+size = [640, 480]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('My Game')
+# initialize clock. used later in the loop.
+clock = pygame.time.Clock()
+ 
+# Loop until the user clicks close button
+done = False
+while done == False:
+    # write event handlers here
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    # write game logic here
+ 
+    # clear the screen before drawing
+    screen.fill((255, 255, 255)) 
+    # write draw code here
+ 
+    # display what’s drawn. this might change.
+    pygame.display.update()
+    # run at 20 fps
+    clock.tick(20)
+ 
+# close the window and quit
+pygame.quit()
